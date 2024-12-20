11de55320ced84cbb27d0928c2d64afdb106eade
PARTH-1712 <risingplayer.007@gmail.com>
2024-12-20 19:34:25 +0530

    QUES 1

--- Introductory/Weird_Algorithm.cpp
+++ Introductory/Weird_Algorithm.cpp
@@ -1,1 +1,41 @@
-#include <iostream>
+#include <iostream> 
+using namespace std;
+
+long long int weird_algorithm(long long int n){
+    if(n % 2 == 0) {
+        return n / 2; 
+    } else {
+        return (n * 3) + 1;  
+    }
+}
+
+int main(){
+    long long int n;
+    cin >> n;
+
+    
+    while (n != 1) {
+        cout << n << " ";  
+        n = weird_algorithm(n);  
+    }
+    cout << n << endl;  
+}
+
+
+/*int main(){
+    long long int n;
+    cin>>n;
+    while(n!=1){
+        cout << n << " ";
+        if(n%2==0){
+            n = n/2;
+        }
+        else{
+            n = (n*3)+1;
+        }
+    }
+    cout << n << endl;
+    
+   
+}*/
+
